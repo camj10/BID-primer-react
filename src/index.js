0886d173041dbb5f96cefac93b26149f10442e18
camj10 <camilajisel06@gmail.com>
2023-01-16 00:54:31 -0300

    Agregar el texto de Hello y la lista

--- src/index.js
+++ src/index.js
@@ -6,12 +6,24 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <div>
+    <h1>Hola Dojo!</h1>
+    <h2>Things I need to do:</h2>
+  <ul>
+    <li>Learn react</li>
+    <li>Climb Mt. Everest</li>
+    <li>Run a marathon</li>
+    <li>Feed the dogs</li>
+  </ul>
+  </div>
+  
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
